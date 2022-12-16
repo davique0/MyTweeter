e83171057062e2d088faf215b9e31ba7f2615b87
davique0 <davique@gmail.com>
2022-12-15 19:03:07 -0500

    Remove hard coded data, cleaning

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
-  // let numChar = Number($('.counter').val());
   $('#tweet-text').on('input', function() {
     let numChar = 140 - this.value.length;
-    console.log($(this).parent().find('.counter'))
+    // console.log($(this).parent().find('.counter'))
     const counter = $(this).parent().find('.counter')
     counter.text(numChar)
     if (numChar > 0) {
